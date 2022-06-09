9856854857029546675c7a01fd983c72031b1c3a
mled-v <matthewtledford@gmail.com>
2022-06-09 13:49:26 -0500

    Updated version of rock paper scissors with 5 rounds

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// This function will randomly select either rock, paper, or scissors
+
 function computerPlay() {
     const number = Math.random() * 100
     if (number <= 33) {
@@ -9,21 +11,48 @@ function computerPlay() {
     }
 }
 
+//  This function put the play selection up against the computer selection from the computerPlay function
 
 function playRound (playerSelection, computerSelection){
-    if ( (playerSelection === 'Rock' && computerSelection === 'Rock') || 
-         (playerSelection === 'Paper' && computerSelection === 'Paper') ||
-         (playerSelection === 'Scissors' && computerSelection === 'Scissors') ) {
+    if ( playerSelection === computerSelection ) {
     return('tie!')
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         return('Rock loses to Paper!')
     }  else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         return ('Rock beats Scissors')
-    }
-        
+    }   else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return ('Paper loses to Scissors')
+    }   else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return ('Paper beats Rock')
+    }   else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return ('Scissors loses to Rock')
+    }   else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return ('Scissors beats Paper')
+    } 
+}
+ 
 
+
+
+
+
+// This function prompts the user for a selection of either rock, paper, or scissors and then inputs it in the playRound function
+
+function game () {
+    let playerSelection = prompt ('Rock, Paper, or Scissors?');
+    let computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    
 
 }
-const playerSelection = 'Rock';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+
+
+
+
+
+
+// This function plays the round 5 times. 
+
+for (let i = 1; i <= 5; i++) {
+    game()
+}
